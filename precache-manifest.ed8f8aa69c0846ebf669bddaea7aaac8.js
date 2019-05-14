550610e9f94f08dd5a1622a095fd02da6e1db9ca
Quyen Do <quyen.do@ntq-solution.com.vn>
2019-05-14 09:10:42 +0700

    Updates

---
+++ precache-manifest.ed8f8aa69c0846ebf669bddaea7aaac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23b4a968620e08313a994ea236c7ab0",
+    "url": "/base-project-reactjs/index.html"
+  },
+  {
+    "revision": "dea09d03b045e9bb9404",
+    "url": "/base-project-reactjs/static/css/2.4358233e.chunk.css"
+  },
+  {
+    "revision": "350d0c5a87206ef0802e",
+    "url": "/base-project-reactjs/static/css/main.b22c0019.chunk.css"
+  },
+  {
+    "revision": "dea09d03b045e9bb9404",
+    "url": "/base-project-reactjs/static/js/2.2bb5f16f.chunk.js"
+  },
+  {
+    "revision": "350d0c5a87206ef0802e",
+    "url": "/base-project-reactjs/static/js/main.a0363f87.chunk.js"
+  },
+  {
+    "revision": "1141273c498984b6160a",
+    "url": "/base-project-reactjs/static/js/runtime~main.c3193bc2.js"
+  }
+]);
--- src/configs/config.js
+++
@@ -1,1 +0,0 @@
-export const apiURL = 'http://5c9044f28447f30014cb83de.mockapi.io/';
